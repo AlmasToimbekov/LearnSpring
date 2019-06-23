9495cbdcc16d2f12e192039ae4d88c55ba0a5e83
Almas <atoimbekov@gmail.com>
2019-06-23 09:55:27 +0600

    Test cherry-pick

--- src/main/java/my_project/interfaces/ClassicalMusic.java
+++ src/main/java/my_project/interfaces/ClassicalMusic.java
@@ -1,6 +1,20 @@
 package my_project.interfaces;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    public void initClassical() {
+        System.out.println("Classical is being initiated");
+    }
+
+    public void destroyClassical() {
+        System.out.println("Classical is being destroyed");
+    }
+
     @Override
     public String getSong() {
         return "Sonata";
